8314bd210585137765048640ab8a184d2a6f1af1
Lena <me@lena.place>
2023-10-23 20:33:52 +0200

    lifetime

--- freenect-async/src/video.rs
+++ freenect-async/src/video.rs
@@ -1,3 +1,5 @@
+use std::marker::PhantomData;
+
 use crate::{
     context::{
         FreenectDeviceMode, FreenectDeviceReady, FreenectReadyAll, FreenectReadyVideo,
@@ -46,4 +48,44 @@ where
 
         Ok(())
     }
+
+    pub fn start_video_stream<'b>(&'b mut self) -> Result<VideoStream<'a, 'b, D>, FreenectError> {
+        Ok(VideoStream { device: self })
+    }
+}
+
+pub struct VideoStream<'a, 'b, D: FreenectDeviceReady + FreenectDeviceMode + FreenectVideo> {
+    // keep this private
+    device: &'b mut FreenectDevice<'a, D>
+}
+
+impl<'a, 'b, D: FreenectDeviceReady + FreenectDeviceMode + FreenectVideo> VideoStream<'a, 'b, D> {
+    
+    pub fn dev_ref(&'b self) -> &'b FreenectDevice<'a, D> {
+        self.device
+    }
+
+    pub async fn try_read_depth_frame(&mut self) -> Result<Option<DepthFrame>, FreenectError> {
+        unsafe { 
+            let res = freenect_sys::freenect_process_events_timeout(self.device.context.inner, std::mem::zeroed());
+        }
+        todo!()
+    }
+
+    pub async fn try_read_camera_frame(&mut self) -> Result<Option<CameraFrame>, FreenectError> {
+        unsafe { 
+            let res = freenect_sys::freenect_process_events_timeout(self.device.context.inner, std::mem::zeroed());
+        }
+        todo!()
+    }
+}
+
+pub struct DepthFrame<'c> {
+    timestamp: u32,
+    data: &'c [u16]
+}
+
+pub struct CameraFrame<'c> {
+    timestamp: u32,
+    data: &'c [u8]
 }
